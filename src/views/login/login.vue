752333ff4950f97fc5c82f0a91619ce89f3a472a
coolxiaohao <1779084072@qq.com>
2020-04-19 22:33:51 +0800

    update login page

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="login-container">
+        <!--粒子动画插件-->
         <vue-particles
                 style="width: 100%;height: 100%"
                 color="#dedede"
@@ -20,19 +21,23 @@
         >
             <!--            <div class="login-page"></div>-->
         </vue-particles>
+        <!--登录表单-->
         <el-form class="login-form" autoComplete="on" :model="loginForm" :rules="loginRules" ref="loginForm"
                  label-position="left">
             <h3 class="title">绘梦商城后台登录</h3>
+            <!--用户名-->
             <el-form-item prop="username">
                 <el-input name="username" type="text" v-model="loginForm.username" autoComplete="on"
                           placeholder="username"/>
             </el-form-item>
+            <!--密码-->
             <el-form-item prop="password">
                 <el-input name="password" :type="pwdType" @keyup.enter.native="handleLogin"
                           v-model="loginForm.password"
                           autoComplete="on"
                           placeholder="password"/>
             </el-form-item>
+            <!--验证码-->
             <el-form-item prop="varify" class="varify-div">
                 <el-row type="flex">
                     <el-col :span="17">
@@ -42,10 +47,11 @@
                                   placeholder="varify"/>
                     </el-col>
                     <el-col :span="7">
-                        <el-image class="varifyImg" :src="varifyImg"></el-image>
+                        <el-image class="varifyImg" @click="generateValidateCode" :src="varifyImg"></el-image>
                     </el-col>
                 </el-row>
             </el-form-item>
+            <!--登录按钮-->
             <el-form-item>
                 <el-button type="primary" style="width:100%;" @click.native.prevent="handleLogin">
                     登录
@@ -56,11 +62,12 @@
 </template>
 
 <script>
+    /*验证用户名的方法*/
     import {isvalidUsername} from '@/util'
-
     export default {
         name: 'login',
         data() {
+            /*验证用户名*/
             const validateUsername = (rule, value, callback) => {
                 if (!isvalidUsername(value)) {
                     callback(new Error('请输入正确的用户名'))
@@ -68,6 +75,7 @@
                     callback()
                 }
             }
+            /*验证密码*/
             const validatePass = (rule, value, callback) => {
                 if (value.length < 5) {
                     callback(new Error('密码不能小于5位'))
@@ -90,6 +98,7 @@
             }
         },
         methods: {
+            /*显示密码*/
             showPwd() {
                 if (this.pwdType === 'password') {
                     this.pwdType = ''
@@ -97,6 +106,7 @@
                     this.pwdType = 'password'
                 }
             },
+            /*登录的方法*/
             handleLogin() {
                 this.$refs.loginForm.validate(valid => {
                     if (valid) {
@@ -112,6 +122,7 @@
                     }
                 })
             },
+            /*获取验证码的方法*/
             generateValidateCode() {
                 this.$store.dispatch('getValidateCode').then((res) => {
                     let data = res.data;
@@ -124,6 +135,7 @@
         },
         //页面构造前
         mounted: function () {
+            /*调用登录前的方法*/
             this.generateValidateCode();
         }
     }
